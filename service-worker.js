09463e0a7d5b5a66f3e0e9c6db1422a3a53b57aa
iroilanhv <roilanhv@icloud.com>
2026-09-03 18:06:12 -0300

    V 0.83 Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,5 @@
-const C='calor-board-v081';const A=['./','./index.html','./cards.json','./manifest.webmanifest'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
+const C='calor-board-v083';
+const A=['./','./index.html','./cards.json','./manifest.webmanifest'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A)).then(()=>self.skipWaiting())));
+self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x)))).then(()=>self.clients.claim())));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
